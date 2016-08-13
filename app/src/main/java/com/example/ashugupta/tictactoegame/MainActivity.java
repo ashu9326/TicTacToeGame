27e4eee26529e264b55682117c032f38176d3e5e
ashugupta <ashu9326@gmail.com>
2016-08-14 02:56:38 +0530

    first module

---
+++ app/src/main/java/com/example/ashugupta/tictactoegame/MainActivity.java
@@ -0,0 +1,289 @@
+package com.example.ashugupta.tictactoegame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button b1, b2, b3, b4, b5, b6, b7, b8, b9;
+    int turn=1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1 = (Button) findViewById(R.id.b1);
+        b2 = (Button) findViewById(R.id.b2);
+        b3 = (Button) findViewById(R.id.b3);
+        b4 = (Button) findViewById(R.id.b4);
+        b5 = (Button) findViewById(R.id.b5);
+        b6 = (Button) findViewById(R.id.b6);
+        b7 = (Button) findViewById(R.id.b7);
+        b8 = (Button) findViewById(R.id.b8);
+        b9 = (Button) findViewById(R.id.b9);
+
+        turn = 1;
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b1.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b1.setText("X");
+                    } else if (turn == 2) {
+                        turn = 1;
+                        b1.setText("0");
+                    }
+                }
+                endGameCheck();
+            }
+        });
+
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b2.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b2.setText("X");
+                    } else if (turn == 2) {
+                        turn = 1;
+                        b2.setText("0");
+                    }
+                }
+                endGameCheck();
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b3.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b3.setText("X");
+
+                    } else if (turn == 2) {
+                        turn = 1;
+                        b3.setText("0");
+                    }
+                }
+                endGameCheck();
+            }
+        });
+
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b4.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b4.setText("X");
+
+
+                    } else if (turn == 2) {
+                        turn = 1;
+                        b4.setText("0");
+                    }
+                }
+                endGameCheck();
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b5.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b5.setText("X");
+
+
+                } else if (turn == 2) {
+                        turn = 1;
+                        b5.setText("0");
+                    }   }
+                endGameCheck();
+            }
+        });
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b6.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b6.setText("X");
+
+                    } else if (turn == 2) {
+                        turn = 1;
+                        b6.setText("0");
+                    }
+                }
+                endGameCheck();
+            }
+        });
+
+
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b7.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b7.setText("X");
+
+
+                    } else if (turn == 2) {
+                        turn = 1;
+                        b7.setText("0");
+                    }
+                }
+                endGameCheck();
+            }
+        });
+
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b8.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b8.setText("X");
+
+                } else if (turn == 2) {
+                        turn = 1;
+                        b8.setText("0");
+                    }
+                }
+                endGameCheck();
+            }
+        });
+
+
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b9.getText().toString().equals("")) {
+                    if (turn == 1) {
+                        turn = 2;
+                        b9.setText("X");
+
+                    } else if (turn == 2) {
+                        turn = 1;
+                        b9.setText("0");
+                    }
+                }
+                endGameCheck();
+            }
+        });
+
+    }
+
+    public void endGameCheck() {
+        String a, b, c, d, e, f, g, h, i;
+        boolean end = false;
+
+            a = b1.getText().toString();
+        b = b2.getText().toString();
+        c = b3.getText().toString();
+
+        d = b4.getText().toString();
+        e = b5.getText().toString();
+        f = b6.getText().toString();
+
+        g = b7.getText().toString();
+        h = b8.getText().toString();
+        i = b9.getText().toString();
+
+        //  if(a.equals(b.equals(c)) && !a.isEmpty())
+        if (a.equals("X") && b.equals("X") && c.equals("X")) {
+            Toast.makeText(MainActivity.this, "Winner player is X ", Toast.LENGTH_SHORT).show();
+            end = true;
+        }
+        if (d.equals("X") && e.equals("X") && f.equals("X")) {
+            Toast.makeText(MainActivity.this, "Winner player is X ", Toast.LENGTH_SHORT).show();
+            end = true;
+        }
+        if (g.equals("X") && h.equals("X") && i.equals("X")) {
+            Toast.makeText(MainActivity.this, "Winner player is X ", Toast.LENGTH_SHORT).show();
+            end = true;
+        }
+        if (a.equals("X") && d.equals("X") && g.equals("X")) {
+            Toast.makeText(MainActivity.this, "Winner player is X ", Toast.LENGTH_SHORT).show();
+            end = true;
+        }
+        if (b.equals("X") && e.equals("X") && h.equals("X")) {
+            Toast.makeText(MainActivity.this, "Winner player is X ", Toast.LENGTH_SHORT).show();
+            end = true;
+        }
+        if (c.equals("X") && f.equals("X") && i.equals("X")) {
+            Toast.makeText(MainActivity.this, "Winner player is X ", Toast.LENGTH_SHORT).show();
+            end = true;
+        }
+
+        if (a.equals("X") && e.equals("X") && i.equals("X")) {
+            Toast.makeText(MainActivity.this, "Winner player is X ", Toast.LENGTH_SHORT).show();
+            end = true;
+        }
+        if (c.equals("X") && e.equals("X") && g.equals("X")) {
+            Toast.makeText(MainActivity.this, "Winner player is X ", Toast.LENGTH_SHORT).show();
+            end = true;
+        }
+
+            if (a.equals("0") && b.equals("0") && c.equals("0")) {
+                Toast.makeText(MainActivity.this, "Winner player is 0", Toast.LENGTH_SHORT).show();
+                end = true;
+            }
+            if (d.equals("0") && e.equals("0") && f.equals("0")) {
+                Toast.makeText(MainActivity.this, "Winner player is 0 ", Toast.LENGTH_SHORT).show();
+                end = true;
+            }
+            if (g.equals("0") && h.equals("0") && i.equals("0")) {
+                Toast.makeText(MainActivity.this, "Winner player is 0 ", Toast.LENGTH_SHORT).show();
+                end = true;
+            }
+            if (a.equals("0") && d.equals("0") && g.equals("0")) {
+                Toast.makeText(MainActivity.this, "Winner player is 0 ", Toast.LENGTH_SHORT).show();
+                end = true;
+            }
+            if (b.equals("0") && e.equals("0") && h.equals("0")) {
+                Toast.makeText(MainActivity.this, "Winner player is 0 ", Toast.LENGTH_SHORT).show();
+                end = true;
+            }
+            if (c.equals("0") && f.equals("0") && i.equals("0")) {
+                Toast.makeText(MainActivity.this, "Winner player is 0 ", Toast.LENGTH_SHORT).show();
+                end = true;
+            }
+            if (a.equals("0") && e.equals("0") && i.equals("0")) {
+                Toast.makeText(MainActivity.this, "Winner player is 0 ", Toast.LENGTH_SHORT).show();
+                end = true;
+            }
+            if (c.equals("0") && e.equals("0") && g.equals("0")) {
+                Toast.makeText(MainActivity.this, "Winner player is 0 ", Toast.LENGTH_SHORT).show();
+                end = true;
+            }
+
+            if (end) {
+                b1.setEnabled(false);
+                b2.setEnabled(false);
+                b3.setEnabled(false);
+                b4.setEnabled(false);
+                b5.setEnabled(false);
+                b6.setEnabled(false);
+                b7.setEnabled(false);
+                b8.setEnabled(false);
+                b9.setEnabled(false);
+            }
+
+        }
+    }
+
+
+
+
